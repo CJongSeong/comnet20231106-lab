730c07ab26c4c0907f2e425bf7852f1b79c5b5a1
CJongSeong <cjs6460@gmail.com>
2023-11-10 23:28:26 +0900

    first

---
+++ udp-receiver.py
@@ -0,0 +1,13 @@
+import socket
+
+UDP_IP = '0.0.0.0'
+UDP_PORT = 5001
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+print("UDP Server started !")
+while True:
+    data, addr = sock.recvfrom(1024)
+    print("received message: ", data.decode('utf8'), " from ", addr)
+    sock.sendto(data, addr)
